92ae61ab8bfd89dd3c27e5c5c0c29df15dcca026
xernyuii <hfutxc.xern@gmail.com>
2017-02-10 18:58:21 +0800

    add

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse,Http404
 import datetime
-
+from django.template import Template, Context
 
 #get time
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    t = Template("<html><body>It is now {{ current_date }}.</body></html>")
+    html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
 
 
